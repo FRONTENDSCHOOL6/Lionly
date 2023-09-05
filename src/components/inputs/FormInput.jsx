1186a3f1a73064613c2f91cb9ed23562bbc83668
leejisu <ejeesoo97@gmail.com>
2023-09-05 23:41:18 +0900

    [Feat] 회원가입 input 최소/최대 글자수 지정 및 유효성 검사 코드 구현중 (2023/09/05) #[15]

--- src/components/inputs/FormInput.jsx
+++ src/components/inputs/FormInput.jsx
@@ -1,20 +1,69 @@
-import { oneOf, string } from 'prop-types';
-import { useRef, useId } from 'react';
+import { oneOf, string, func } from 'prop-types';
+import { useRef, useId, useState } from 'react';
+import { nameReg, idReg, nickNameReg, pwReg } from '@/utils/validation';
 
-function FormInput({ type, name = null, label, placeholder }) {
+function FormInput({
+  type,
+  name = null,
+  label,
+  placeholder,
+  errorMessage,
+  minLength,
+  maxLength,
+}) {
   const id = useId();
   const inputRef = useRef(null);
+
+  const [isErrorShow, setIsErrorShow] = useState(false);
 
   const handleInput = (e) => {
     e.preventDefault();
     const inputValue = inputRef.current.value;
     console.log(inputValue);
+
+    let isValid = false;
+
+    switch (name) {
+      case 'userName':
+        isValid = nameReg(inputValue);
+        break;
+      case 'userId':
+        isValid = idReg(inputValue);
+        break;
+      case 'userPassWord':
+        isValid = pwReg(inputValue);
+        break;
+      case 'userPasswordCheck':
+        isValid = pwReg(inputValue);
+        break;
+      case 'userNickName':
+        isValid = nickNameReg(inputValue);
+        break;
+      case 'ansWer':
+        isValid = inputValue === '카레빵맨';
+        break;
+      default:
+        isValid = true;
+    }
+
+    if (isValid || inputValue === '') {
+      console.log(`유효한 ${type}입니다.`, inputValue);
+      setIsErrorShow(false);
+    } else {
+      console.error(`유효하지 않은 ${type}입니다.`, inputValue);
+      setIsErrorShow(true);
+    }
   };
 
   return (
-    <div>
-      <form action="/" method="post" onSubmit={handleInput}>
-        <fieldset className="bg-lionly-primary-color">
+    <div className="bg-lionly-primary-color">
+      <form
+        action="/"
+        method="post"
+        onSubmit={handleInput}
+        onChange={handleInput}
+      >
+        <fieldset>
           <label
             htmlFor={id}
             className="block text-lionly-sm-bold text-lionly-white"
@@ -29,17 +78,31 @@ function FormInput({ type, name = null, label, placeholder }) {
             className="h-11 w-[250px] rounded border border-lionly-white bg-transparent px-5 py-3 text-lionly-sm outline-none placeholder:text-lionly-white"
             ref={inputRef}
             autoComplete="off"
+            minLength={minLength}
+            maxLength={maxLength}
           />
         </fieldset>
       </form>
+      <span
+        className={`text-lionly-xs text-lionly-red ${
+          isErrorShow ? 'block' : 'hidden'
+        }`}
+      >
+        {errorMessage}
+      </span>
     </div>
   );
 }
 
 FormInput.propTypes = {
-  type: oneOf(['text', 'password', 'number', 'email', 'search']),
+  type: oneOf(['text', 'password', 'number', 'email', 'search', 'nickname']),
   name: string.isRequired,
   label: string.isRequired,
   placeholder: string.isRequired,
+  errorMessage: string.isRequired,
+  handleInput: func,
+  minLength: string,
+  maxLength: string,
 };
+
 export default FormInput;
