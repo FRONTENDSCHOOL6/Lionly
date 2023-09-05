27f023f846f364512ffe05f08fb2b88810c79335
seumomo <xo0207@naver.com>
2023-09-05 22:02:38 +0900

    [Refactor] Channel Button List 코드 리팩토링 (2023/09/05)
    상태 없이 기능 구현할 수 있을 것으로 보여, Context 및 State 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,7 @@
-import ChannelButton from './components/ChannelButton';
-import ChannelButtonContainer from './components/ChannelButtonContainer';
-import ChannelProvider, { useChannel } from './contexts/Channel';
+import ChannelButtonList from './components/ChannelButtonList';
 
 function App() {
-  const { select, handleChangeChannel } = useChannel();
-  return (
-    <ChannelButtonContainer>
-      <ChannelButton
-        channelName={'전체 게시글'}
-        defaultChecked={false}
-        onClick={handleChangeChannel}
-      />
-      <ChannelButton
-        channelName={'일상방'}
-        defaultChecked={false}
-        onClick={handleChangeChannel}
-      />
-      <ChannelButton
-        channelName={'힐링방'}
-        defaultChecked={false}
-        onClick={handleChangeChannel}
-      />
-      <ChannelButton
-        channelName={'취업방'}
-        defaultChecked={false}
-        onClick={handleChangeChannel}
-      />
-    </ChannelButtonContainer>
-  );
+  return <ChannelButtonList />;
 }
 
 export default App;
