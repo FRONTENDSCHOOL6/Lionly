b89e998a9927880fd071d0bed9543e44d6400533
seumomo <xo0207@naver.com>
2023-09-06 07:57:40 +0900

    [Add, File] 프로필 변경 버튼 hover 시 디자인 요소 적용 및 아이콘 추가 (2023/09/06)

--- src/components/Button/ProfileImage.jsx
+++ src/components/Button/ProfileImage.jsx
@@ -1,5 +1,6 @@
-import { func, node, string } from 'prop-types';
 import useProfileImage from '@/hooks/useProfileImage';
+import { func, node, string } from 'prop-types';
+import { ReactComponent as CameraIcon } from '@/assets/camera_myProfile.svg';
 
 function ProfileImage({ handleInputClick, children }) {
   const { profileImageURL } = useProfileImage();
@@ -10,13 +11,19 @@ function ProfileImage({ handleInputClick, children }) {
       onClick={handleInputClick}
       tabIndex={0}
     >
-      {profileImageURL === undefined ? null : (
-        <img
-          src={profileImageURL}
-          alt="프로필 이미지"
-          className="h-full w-full rounded-full border border-lionly-gray-4 bg-cover bg-no-repeat"
-        />
-      )}
+      <img
+        src={profileImageURL}
+        alt="프로필 이미지"
+        className="hover: h-full w-full rounded-full border-2 border-lionly-gray-4 bg-cover bg-no-repeat"
+      />
+      {handleInputClick ? (
+        <div className="group absolute top-0 h-full w-full rounded-full border-2 text-lionly-sm-bold transition-all hover:bg-lionly-black hover:bg-opacity-[0.85]">
+          <CameraIcon className="absolute left-[calc(50%-12px)] top-2.5 hidden w-6 group-hover:block" />
+          <span className="absolute bottom-3 left-[calc(50%-22.135px)] hidden text-lionly-white group-hover:block">
+            사진 변경
+          </span>
+        </div>
+      ) : null}
       {children}
     </div>
   );
