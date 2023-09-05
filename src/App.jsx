f62140673d5e3e450d90bbd65fc0ad0508ee77cd
chowonn <hcw12321@gmail.com>
2023-09-05 23:18:50 +0900

    [Add] CreateFeed 컴포넌트 추가 (2023/09/05)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import CreateFeed from '@/components/CreateFeed';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <CreateFeed />
+    </div>
+  );
 }
 
 export default App;
