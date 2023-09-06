a892cb3a43c7f17e23f2212f36757aa4b2e46ad9
itzwe <itzwewewe@naver.com>
2023-09-06 11:04:48 +0900

    [Modify] 이미지 이름 변경, 버튼 컴포넌트 수정 마진 값 추가

--- src/components/Button/WritingButton.jsx
+++ src/components/Button/WritingButton.jsx
@@ -1,8 +1,8 @@
-import pen from '@/assets/pen.svg'
+import pen from '@/assets/pen_Writing.svg'
 
 function WritingButton() {
   return (
-    <button className='w-[88px] h-[34px] text-lionly-md rounded-lg bg-lionly-secondary-color text-lionly-white flex items-center justify-center gap-2'>
+    <button className='w-[88px] h-[34px] text-lionly-md rounded-lg bg-lionly-secondary-color text-lionly-white flex items-center justify-center gap-2 mr-[10px]'>
       <img src={pen} alt="글 작성하기"/>
       글쓰기
     </button>
