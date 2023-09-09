e59b9f25f010f5782a49ed53d62818b577fbcace
seumomo <xo0207@naver.com>
2023-09-10 06:50:38 +0900

    [Refactor] 글쓰기 버튼 navigate 설정 (2023/09/10)

--- src/components/Button/WritingButton.jsx
+++ src/components/Button/WritingButton.jsx
@@ -1,16 +1,20 @@
 import { ReactComponent as PenSVG } from '@/assets/pen_Writing.svg';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function WritingButton() {
+  const navigate = useNavigate();
+
   return (
-    <Link
-      to="/writing"
-      role="button"
+    <button
+      type="button"
+      onClick={() => {
+        navigate('/writing');
+      }}
       className="flex h-[34px] w-[88px] items-center justify-center gap-2 rounded-lg bg-lionly-secondary-color text-lionly-md text-lionly-white"
     >
       <PenSVG />
       글쓰기
-    </Link>
+    </button>
   );
 }
 
