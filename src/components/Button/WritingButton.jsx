a28160fb9b303920a55b9190286698ddaef468cc
seumomo <xo0207@naver.com>
2023-09-09 20:16:11 +0900

    [Refactor] 글쓰기 버튼 a태그로 리팩토링 (2023/09/09)

--- src/components/Button/WritingButton.jsx
+++ src/components/Button/WritingButton.jsx
@@ -1,12 +1,17 @@
-import pen from '@/assets/pen_Writing.svg'
+import { ReactComponent as PenSVG } from '@/assets/pen_Writing.svg';
+import { Link } from 'react-router-dom';
 
 function WritingButton() {
   return (
-    <button className='w-[88px] h-[34px] text-lionly-md rounded-lg bg-lionly-secondary-color text-lionly-white flex items-center justify-center gap-2 mr-[10px]'>
-      <img src={pen} alt="글 작성하기"/>
+    <Link
+      to="/writing"
+      role="button"
+      className="flex h-[34px] w-[88px] items-center justify-center gap-2 rounded-lg bg-lionly-secondary-color text-lionly-md text-lionly-white"
+    >
+      <PenSVG />
       글쓰기
-    </button>
-  )
+    </Link>
+  );
 }
 
 export default WritingButton;
