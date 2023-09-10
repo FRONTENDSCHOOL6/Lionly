1237d48740001b3ca352ebc6bdbb56bf0788148f
chowonn <hcw12321@gmail.com>
2023-09-10 15:18:19 +0900

    [Modify] useState 주석 처리 (2023/09/10)

--- src/components/layout/FeedHeader.jsx
+++ src/components/layout/FeedHeader.jsx
@@ -2,12 +2,12 @@ import feedImg from '@/assets/feed_Header.svg';
 import WritingButton from '../button/WritingButton';
 import LogoutButton from '../button/LogoutButton';
 import PocketBase from 'pocketbase';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 function FeedHeader() {
   const pb = new PocketBase('https://lionly.pockethost.io/');
 
-  let [img,setImg] = useState('');
+  // let [img,setImg] = useState('');
   const readRecordOne = async (collectionName, id) => {
     try {
       return await pb.collection(collectionName).getOne(id);
@@ -15,14 +15,12 @@ function FeedHeader() {
       throw new Error(error.message);
     }
   };
-  
+
   async function fetchAndLogData() {
     try {
       const result = await readRecordOne('users', 'v20jyys9v37stq8');
-      console.log(result.profile_image
-        ); 
+      console.log(result.profile_image);
       // setImg(result.profile_image);
-      
     } catch (error) {
       console.error(error);
     }
@@ -58,7 +56,9 @@ function FeedHeader() {
       <div className="mt-10 flex justify-center pb-3 pl-1 text-white">
         <button className="relative mr-[19px] h-[70px] w-[70px] rounded-full border-2 border-white">
           <img
-            src={'https://lionly.pockethost.io/api/files/_pb_users_auth_/v20jyys9v37stq8/i6Jagc3jWg_xV17i9Ct9Z.jpg?token='}
+            src={
+              'https://lionly.pockethost.io/api/files/_pb_users_auth_/v20jyys9v37stq8/i6Jagc3jWg_xV17i9Ct9Z.jpg?token='
+            }
             alt=""
             className="h-[70px] w-[70px] rounded-full"
           />
