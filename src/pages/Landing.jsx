35e3246eb2f1d676cc52a6ea1f6f3d0217ca6ce6
seumomo <xo0207@naver.com>
2023-09-11 08:37:38 +0900

    [Refactor] 랜딩페이지 및 BigButton 리팩토링 (2023/09/11)
    1. BigButton에서 text와 destination을 인수로 받아 버튼 안에 출력될 text와 클릭 이동할 경로를 설정합니다.
    2. w-full을 설정하여 뷰포트의 너비가 넓어질 때 버튼의 사이즈도 같이 늘어납니다.

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,12 @@
-// import BigButton from '@/components/button/BigButton';
+import BigButton from '@/components/button/BigButton';
 
 function Landing() {
-  return <div>Landing</div>;
+  return (
+    <div className="mt-[162px] flex w-full justify-center">
+      <h2 className="sr-only">랜딩 페이지</h2>
+      <BigButton text="시작하기" destination="/signin" />
+    </div>
+  );
 }
 
 export default Landing;
