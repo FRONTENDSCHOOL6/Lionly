4d602eb71f44238dc6a86ce9e8990a10c9f53707
seumomo <xo0207@naver.com>
2023-09-10 20:00:03 +0900

    [Feat] user의 nickname 불러오기 구현 (2023/09/10)

--- src/components/layout/FeedHeader.jsx
+++ src/components/layout/FeedHeader.jsx
@@ -1,10 +1,25 @@
+import getProfileNickname from '@/api/getProfileNickname';
+import { handleTabArrowControl } from '@/utils';
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LogoutButton, ProfileImage, WritingButton } from '../button';
 import { ReactComponent as LionHeadLogo } from '/src/assets/lionHeadLogo_common.svg';
-import { handleTabArrowControl } from '@/utils';
 
 function FeedHeader() {
+  const [nickname, setNickname] = useState(null);
   const navigate = useNavigate();
+  const { status } = useQuery({
+    queryKey: ['nickname'],
+    queryFn: getProfileNickname,
+    onSuccess: (nickname) => {
+      setNickname(nickname);
+    },
+
+    retry: 2,
+  });
+
+  console.log('FeedHeader: ', status);
 
   return (
     <div className="bg-lionly-primary-color px-4">
@@ -39,7 +54,7 @@ function FeedHeader() {
       <div className="mt-8 flex justify-center gap-x-8 pb-[30px] text-lionly-white">
         <ProfileImage />
         <div>
-          <span className="text-lionly-lg">두부 왕자</span>
+          <span className="text-lionly-lg">{nickname}</span>
           <div className="flex gap-x-[10px]">
             <WritingButton />
             <LogoutButton />
