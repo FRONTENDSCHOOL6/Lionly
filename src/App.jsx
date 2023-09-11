2df5d29696638ae02055fe409aec2c4e65e786b7
seumomo <xo0207@naver.com>
2023-09-11 10:20:57 +0900

    [Add] HelmetProvider 추가 (2023/09/11)

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import ChannelProvider from './contexts/Channel.jsx';
 import ProfileImageProvider from './contexts/ProfileImage.jsx';
 import router from './routes.jsx';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { HelmetProvider } from 'react-helmet-async';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChannelProvider>
-        <ProfileImageProvider>
-          <RouterProvider router={router} />
-        </ProfileImageProvider>
-      </ChannelProvider>
-    </QueryClientProvider>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChannelProvider>
+          <ProfileImageProvider>
+            <RouterProvider router={router} />
+          </ProfileImageProvider>
+        </ChannelProvider>
+      </QueryClientProvider>
+    </HelmetProvider>
   );
 }
 
