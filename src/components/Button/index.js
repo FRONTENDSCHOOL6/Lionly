05f370a093bc74d479862636aec3d4e6ff9e1051
TAEIL KIM <xo0207@naver.com>
2023-09-11 09:22:07 +0900

    [PR] 공통 레이아웃 수정, BigButton 컴포넌트 수정, 에러 페이지 디자인 추가 (#54)
    * [Feat] 채널 변경 버튼 컴포넌트에 접근성을 고려한 키보드 포커스 이동 구현
    
    * [Add] Spinner 컴포넌트 추가 (2023/09/07)
    
    * [Add] Spinner 컴포넌트 추가 (2023/09/07)
    
    * [Feat] 채널의 Data를 GET하여 버튼이 자동 생성되는 기능 구현
    
    * [Refactor] Channels Context를 useQuery를 사용하여 리팩토링 (2023/09/08)
    
    * [Add, Feat] users 데이터에서 프로필 이미지 URL을 받아와 렌더링 되는 기능 구현 (2023/09/09)
    
    현재는 users의 id를 넣어서 지정한 사용자의 이미지가 렌더링 됨
    
    로그인 된 사용자의 이미지가 렌더링 되는 기능은, 로그인 기능 구현 이후에 구현할 예정
    
    * [Add, Feat, File] 시작하기 페이지 구현 및 공통 레이아웃 구현 (2023/09/09)
    
    1. StartButton 컴포넌트 파일명 수정
    시작하기로만 쓰이는 것이 아니어서 수정되어야 할 것으로 보여,
    크기가 큰 버튼이니 일단 BigButton으로 수정하였습니다.
    
    2. MainLogo 컴포넌트 구현
    - props로 사이즈를 숫자값으로 주시면, width/height 자동 설정됩니다.
    - z-index 속성으로 배경색 입혀지지 않게 했습니다.
    - 로고를 클릭하면 랜딩 페이지로 가도록 했습니다.
    
    3. SmallLogo(머리만 있는 사자) 컴포넌트 구현
    - props로 사이즈를 숫자값으로 주시면, width/height 자동 설정됩니다.
    - z-index 속성으로 배경색 입혀지지 않게 했습니다.
    
    4. 파일명 수정
    Error-page => ErrorPage
    
    5. routes.jsx
    - pages에서 많은 요소들이 import되는데, 비교적 보기 편하게 index.js에서 re-export하도록 했습니다.
    - RootLayout을 구성하여 모든 페이지에 반응형으로 동작하도록 했습니다. => 배경색과 컨텐츠 너비는 설정되어 있습니다.
    - 로그인 전의 페이지들은 추가로 BeforeLogin 레이아웃을 구성하여 페이지마다 title 요소(사자 로고, 페이지 이름)가 변경되도록 했습니다. => 랜딩, 로그인, 회원가입 페이지에서 해당 부분은 만들지 않으셔도 됩니다.
    
    * [Refactor] 글쓰기 버튼 a태그로 리팩토링 (2023/09/09)
    
    * [Refactor] 뷰포트가 줄어들 때 크기가 줄어들지 않도록 최소 너비 설정 (2023/09/09)
    
    * [Refactor] 가운데 고정되도록 flex 부여 (2023/09/09)
    
    * [Refactor] 글쓰기 버튼 navigate 설정 (2023/09/10)
    
    * [Refactor] 채널 선택 시 경로 변경되도록 설정 및 Outlet 설정 (2023/09/10)
    
    * [Refactor] FeedHeader 리팩토링 (2023/09/10)
    
    * [File] Component/button 인덱스 설정 (2023/09/10)
    
    * [Refactor] SVG 이미지에 aria-hidden 설정 (2023/09/10)
    
    * [Feat] FeedHeader에서 로고 클릭 시 홈으로 이동되는 기능 구현 (2023/09/10)
    
    * [Add] 피드 페이지 구성 중 Merge 전 Commit (2023/09/10)
    
    * [Feat] user의 nickname 불러오기 구현 (2023/09/10)
    
    * [Refactor] 랜딩페이지 및 BigButton 리팩토링 (2023/09/11)
    
    1. BigButton에서 text와 destination을 인수로 받아 버튼 안에 출력될 text와 클릭 이동할 경로를 설정합니다.
    2. w-full을 설정하여 뷰포트의 너비가 넓어질 때 버튼의 사이즈도 같이 늘어납니다.
    
    * [Refactor] 로그인 전 공통 레이아웃 수정 (2023/09/11)
    
    * [Add] 에러 페이지 디자인 추가 (2023/09/11)

---
+++ src/components/Button/index.js
@@ -0,0 +1,7 @@
+export { default as BigButton } from './BigButton';
+export { default as ChangeButton } from './ChangeButton';
+export { default as ChannelTab } from './ChannelTab';
+export { default as InsertProfileButton } from './InsertProfileButton';
+export { default as LogoutButton } from './LogoutButton';
+export { default as ProfileImage } from './ProfileImage';
+export { default as WritingButton } from './WritingButton';
