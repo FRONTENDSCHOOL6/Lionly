5b4dba62f78b4cb65bc4acd04563908dc1928790
seumomo <xo0207@naver.com>
2023-09-11 10:22:32 +0900

    [Add] 랜딩 페이지에서 버튼 외부 간격을 위한 px 설정 (2023/09/11)

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,7 +2,7 @@ import BigButton from '@/components/button/BigButton';
 
 function Landing() {
   return (
-    <div className="mt-[162px] flex w-full justify-center">
+    <div className="mt-[162px] flex w-full justify-center px-[35px]">
       <h2 className="sr-only">랜딩 페이지</h2>
       <BigButton text="시작하기" destination="/signin" />
     </div>
