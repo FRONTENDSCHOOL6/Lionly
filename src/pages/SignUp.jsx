3b394e560ad4207ff101efd1ebd23f5c82c008e9
leejisu <ejeesoo97@gmail.com>
2023-09-11 10:42:57 +0900

    [Modify] 버튼 margin-bottom 제거/ 회원가입 input 에 flex-gap 적용하여 간격 조정 (2023/09/11)

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,64 +1,63 @@
 import BigButton from '@/components/Button/BigButton';
 import FormInput from '@/components/input/FormInput';
-// import StartButton from './../components/Button/StartButton';
 
 function SignUp({ text }) {
   return (
-    <div className="px-8">
-      <h2 className="mb-6 mt-[30px] text-lionly-2xl text-lionly-white">
-        회원가입
-      </h2>
+    <div className="flex flex-col gap-y-[26px] px-8">
+      <h2 className="mt-[30px] text-lionly-2xl text-lionly-white">회원가입</h2>
 
-      <FormInput
-        type="text"
-        name="userName"
-        label="이름"
-        placeholder="이름을 입력해주세요."
-        errorMessage="한글 2~5자로 입력해주세요."
-        minLength="2"
-        maxLength="5"
-      />
-      <FormInput
-        type="email"
-        label="아이디"
-        name="userId"
-        placeholder="아이디를 입력해주세요."
-        errorMessage="이메일 형식으로 입력해주세요."
-      />
-      <FormInput
-        type="text"
-        label="닉네임"
-        name="userNickName"
-        placeholder="닉네임을 입력해주세요."
-        errorMessage="한글 3~8자로 입력해주세요."
-        minLength="3"
-        maxLength="8"
-      />
-      <FormInput
-        type="password"
-        label="비밀번호"
-        name="userPassword"
-        placeholder="비밀번호를 입력해주세요."
-        errorMessage="영문, 숫자, 특수문자를 포함한 8~16자로 입력해주세요."
-        minLength="8"
-        maxLength="16"
-      />
-      <FormInput
-        type="password"
-        label="비밀번호 확인"
-        name="userPasswordCheck"
-        placeholder="비밀번호를 다시 입력해주세요."
-        errorMessage="비밀번호가 일치하지 않습니다"
-        minLength="8"
-        maxLength="16"
-      />
-      <FormInput
-        type="text"
-        label="범쌤과 닮은 캐릭터의 이름은?"
-        name="answer"
-        placeholder="정답을 입력해주세요."
-        errorMessage="정답이 일치하지 않습니다."
-      />
+      <div className="flex flex-col gap-y-3">
+        <FormInput
+          type="text"
+          name="userName"
+          label="이름"
+          placeholder="이름을 입력해주세요."
+          errorMessage="한글 2~5자로 입력해주세요."
+          minLength="2"
+          maxLength="5"
+        />
+        <FormInput
+          type="email"
+          label="아이디"
+          name="userId"
+          placeholder="아이디를 입력해주세요."
+          errorMessage="이메일 형식으로 입력해주세요."
+        />
+        <FormInput
+          type="text"
+          label="닉네임"
+          name="userNickName"
+          placeholder="닉네임을 입력해주세요."
+          errorMessage="한글 3~8자로 입력해주세요."
+          minLength="3"
+          maxLength="8"
+        />
+        <FormInput
+          type="password"
+          label="비밀번호"
+          name="userPassword"
+          placeholder="비밀번호를 입력해주세요."
+          errorMessage="영문, 숫자, 특수문자를 포함한 8~16자로 입력해주세요."
+          minLength="8"
+          maxLength="16"
+        />
+        <FormInput
+          type="password"
+          label="비밀번호 확인"
+          name="userPasswordCheck"
+          placeholder="비밀번호를 다시 입력해주세요."
+          errorMessage="비밀번호가 일치하지 않습니다"
+          minLength="8"
+          maxLength="16"
+        />
+        <FormInput
+          type="text"
+          label="범쌤과 닮은 캐릭터의 이름은?"
+          name="answer"
+          placeholder="정답을 입력해주세요."
+          errorMessage="정답이 일치하지 않습니다."
+        />
+      </div>
       <BigButton text="가입하기" />
     </div>
   );
