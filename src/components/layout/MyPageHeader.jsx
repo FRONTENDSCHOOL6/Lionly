f61aa2f1cf5c30e11cc771d1050ad8e999132485
chowonn <hcw12321@gmail.com>
2023-09-12 10:41:18 +0900

    [Add] 마이페이지 헤더 마크업 진행 중 (2023/09/12)

--- src/components/layout/MyPageHeader.jsx
+++ src/components/layout/MyPageHeader.jsx
@@ -1,9 +1,32 @@
 import { ReactComponent as SignOutSVG } from '@/assets/signOut_MyPage.svg';
 import { ReactComponent as ButtonPrevSVG } from '@/assets/buttonPrev_MyPage.svg';
+import ProfileImage from './../Button/ProfileImage';
+import getProfileNickname from '@/api/getProfileNickname';
+import { useQuery } from '@tanstack/react-query';
+import getProfileEmail from '@/api/getProfileEmail';
+// import { BigButton } from '../Button';
 
 function MypPageHeader() {
+  const { data: email } = useQuery(['email'], getProfileEmail, {
+    retry: 3,
+    cacheTime: 5 * 60 * 1000,
+    staleTime: 3 * 1000,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+    refetchOnMount: false,
+  });
+
+  const { data: nickname } = useQuery(['nickname'], getProfileNickname, {
+    retry: 3,
+    cacheTime: 5 * 60 * 1000,
+    staleTime: 3 * 1000,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+    refetchOnMount: false,
+  });
+
   return (
-    <div>
+    <div className="mx-auto">
       <div className="mx-4 mb-[30px] mt-4 flex justify-between bg-lionly-primary-color">
         <button type="button">
           <ButtonPrevSVG />
@@ -13,6 +36,18 @@ function MypPageHeader() {
           <SignOutSVG />
         </button>
       </div>
+
+      <figure className=" ">
+        {/* <img src="" alt="" className="mb-3" /> */}
+        <ProfileImage />
+        <figcaption>
+          <p className="mb-2 mt-3 text-lionly-xs text-lionly-gray-2">
+            <u>{email}</u>
+          </p>
+          <p className="mb-7 text-lionly-lg text-lionly-black">{nickname}</p>
+        </figcaption>
+        {/* <BigButton text="프로필 편집" /> */}
+      </figure>
     </div>
   );
 }
