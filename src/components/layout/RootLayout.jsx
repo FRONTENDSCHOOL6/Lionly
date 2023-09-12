2e16d541e9b2cc201a5f9ffd144d97be376b82ef
seumomo <xo0207@naver.com>
2023-09-13 07:06:52 +0900

    [Add] RootLayout에서 배경 설정 제거하고 index.html에 배경 설정 (2023/09/13)

--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -2,10 +2,8 @@ import { Outlet } from 'react-router-dom';
 
 function RootLayout() {
   return (
-    <div className="h-screen overflow-x-hidden bg-lionly-primary-color">
-      <div className="relative mx-auto max-w-screen-md">
-        <Outlet />
-      </div>
+    <div className="relative mx-auto max-w-screen-md">
+      <Outlet />
     </div>
   );
 }
