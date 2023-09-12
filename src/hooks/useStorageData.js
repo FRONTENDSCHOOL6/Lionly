7a2dc936e50b9752b088bc0d4103b1dc7c878a61
chowonn <hcw12321@gmail.com>
2023-09-13 02:20:03 +0900

    [Refactor] 전달한 키 값 제거 (2023/09/13)

--- src/hooks/useStorageData.js
+++ src/hooks/useStorageData.js
@@ -1,18 +1,13 @@
-import pb from '@/api/pocketbase'
+import pb from '@/api/pocketbase';
 import { useNavigate } from 'react-router-dom';
 
-
-// 중요 ! key는 문자열 string으로 전달해야함
-function useStorageData(key) {
+function useStorageData() {
   const navigate = useNavigate();
-  if(!pb.authStore.isValid){
+  if (!pb.authStore.isValid) {
     navigate('/signin');
   }
-  if(typeof key !== 'string'){
-    throw new Error('key는 string 타입을 줘야 합니다.');
-  }
-  
-  return pb.authStore.model[key];
+
+  return pb.authStore.model;
 }
 
-export default useStorageData
+export default useStorageData;
