d85289f61a1667d571a527e3d1981f60a2117f5f
seumomo <xo0207@naver.com>
2023-09-13 16:11:41 +0900

    [Modify] getPbImageURL파일에서 환경변수 수정 (2023/09/13)

--- src/utils/getPbImageURL.js
+++ src/utils/getPbImageURL.js
@@ -1,8 +1,6 @@
 export const getPbImageURL = (item, fileName = 'image') => {
   if (item[fileName]) {
-    return `${import.meta.env.VITE_PB_API}/files/${item.collectionId}/${
-      item.id
-    }/${item[fileName]}`;
+    return `https://lionly.pockethost.io/files/${item.collectionId}/${item.id}/${item[fileName]}`;
   } else {
     return null;
   }
