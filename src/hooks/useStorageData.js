5aa962864e251028a1e108bdc196909ed699aae5
JiHamPark <74188144+itzwe@users.noreply.github.com>
2023-09-13 11:26:43 +0900

    routes 구조 변경 , useStorageData if 문제거 ,  (#62)
    * [Modify] 이미지 이름 변경, 버튼 컴포넌트 수정
    마진 값 추가
    
    * [Add] Header 컴포넌트 추가
    
    * [File] feed 사자 머리 이미지 추가
    
    * 임시커밋
    
    * 임시커밋
    
    * [Add,File] router 초기 세팅 , pages 파일 생성 (2023/09/07)
    
    * [add]
    [2차 작업] Feed 페이지의 header에서 users의 profile_image와 nickname 불러오기
    
    * [modify] getDate 함수 export, console.log 제거
    
    * [add] debounce 추가
    
    * [add] 로그인 임시커밋
    
    * [modify] 시간 불러오는 함수 수정
    
    * [add] Feed.jsx feedList 컴포넌트 추가
    
    * [Add] App context코드 추가
    로그인이 됐음을 알리는 context 전달 하기위해서 app 상위 컴포넌트로 AuthProvider 전달
    
    * [File] useFeedHeader 제거
    authStore 에서 레코드의 필드를 불러오기에 제거
    
    * [File] 사용자의 데이터를 불러오는 hook
    로그인한 사용자의 데이터를 매개변수로 key만 주면 불러오는 hook 생성
    
    * [Feat] 로그아웃 버튼 기능 추가
    버튼 클릭 시 사용자 인증 제거
    
    * [Feat] 로그인 페이지 기능 구현
    마크업 x 로그인 기능 구현
    
    * [File] 로그인 여부 확인해주는 context
    로그인 시 페이지가 접속 가능하게 해주는 기능을 해주는 context 파일
    
    * [Add] 로그인 컴포넌트 추가
    
    * [File] 계정 찾기 버튼 생성
    
    * [Add] 로그인 마크업 수정
    
    * [Add] 로그인 페이지 마크업 수정
    
    * [modify] bigbutton -> linkbutton 이름 변경 및 props 수정
    
    * [Add] 버튼 type button으로 명시
    
    * [modify] 주석제거
    
    * [Feat,Add] 애니메이션 기능 추가 및 마크업 수정
    
    * [Add] 애니메이션 추가
    
    * .
    
    * [File] protect route 파일 추가
    
    * [modify] if 문 제거
    
    * protect route 패턴 추가

--- src/hooks/useStorageData.js
+++ src/hooks/useStorageData.js
@@ -1,13 +1,9 @@
 import pb from '@/api/pocketbase';
-import { useNavigate } from 'react-router-dom';
 
 function useStorageData() {
-  const navigate = useNavigate();
-  if (!pb.authStore.isValid) {
-    navigate('/signin');
-  }
-
   return pb.authStore.model;
 }
+
+
 
 export default useStorageData;
