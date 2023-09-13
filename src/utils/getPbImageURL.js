dec57d3d1b8bae318da42781fbe9ba8357467123
seumomo <xo0207@naver.com>
2023-09-13 16:16:42 +0900

    [Modify] 환경변수 수정 (2023/09/13)

--- src/utils/getPbImageURL.js
+++ src/utils/getPbImageURL.js
@@ -1,6 +1,6 @@
 export const getPbImageURL = (item, fileName = 'image') => {
   if (item[fileName]) {
-    return `https://lionly.pockethost.io/files/${item.collectionId}/${item.id}/${item[fileName]}`;
+    return `https://lionly.pockethost.io/api/files/${item.collectionId}/${item.id}/${item[fileName]}`;
   } else {
     return null;
   }
