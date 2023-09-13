452ae952f6253aea5ac76f34dd35a3f67746bf64
chowonn <hcw12321@gmail.com>
2023-09-13 10:45:10 +0900

    [Modify] 오타 수정 (2023/09/13)

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,15 +1,17 @@
-import FeedList from '@/components/layout/FeedList';
-import MypPageHeader from '@/components/layout/MypageHeader';
+import MyFeedList from '@/components/layout/MyFeedList';
+import MyPageHeader from '@/components/layout/MyPageHeader';
+import useIsLogin from '@/contexts/AuthProvider';
 
 function MyPage() {
+  useIsLogin();
   return (
-    <div className="w-full ">
-      <MypPageHeader />
+    <div>
+      <MyPageHeader />
       <div className="z-10 h-full bg-lionly-white">
         <p className="mx-4 mb-4 mt-9 border-b-4 border-lionly-black px-11 py-3 text-center text-lionly-lg text-lionly-black">
           내가 쓴 글
         </p>
-        <FeedList />
+        <MyFeedList />
       </div>
     </div>
   );
