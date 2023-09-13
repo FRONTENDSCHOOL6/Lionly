fcfad81ed58ff74368f0032a80335555bed3750d
leejisu <ejeesoo97@gmail.com>
2023-09-13 17:35:40 +0900

    [Modify] helmet 코드 추가 및 text props 제거 (2023/09/13)

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,8 +6,9 @@ import { ClientResponseError } from 'pocketbase';
 import { useState } from 'react';
 import { toast } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 
-function SignUp({ text }) {
+function SignUp() {
   const navigate = useNavigate();
 
   const [userData, setUserData] = useState({
@@ -93,76 +94,84 @@ function SignUp({ text }) {
   };
 
   return (
-    <div className="flex flex-col gap-y-[26px] px-8">
-      <h2 className="mt-[30px] text-lionly-2xl text-lionly-white">회원가입</h2>
+    <>
+      <Helmet>
+        <title>Lionly - SignIn</title>
+      </Helmet>
+      <div className="flex flex-col gap-y-[26px] px-8">
+        <h2 className="mt-[30px] text-lionly-2xl text-lionly-white">
+          회원가입
+        </h2>
 
-      <form onSubmit={handleSignUp} className="flex flex-col gap-y-5">
-        <FormInput
-          type="text"
-          name="name"
-          label="이름"
-          placeholder="이름을 입력해주세요."
-          errorMessage="한글 2~5자로 입력해주세요."
-          minLength="2"
-          maxLength="5"
-          value={userData.name}
-          onChange={handleChange}
-        />
-        <FormInput
-          type="email"
-          label="아이디"
-          name="userid"
-          placeholder="이메일 형식으로 입력해주세요."
-          errorMessage="올바른 이메일 형식이 아닙니다."
-          value={userData.userid}
-          onChange={handleChange}
-        />
-        <FormInput
-          type="text"
-          label="닉네임"
-          name="usernickname"
-          placeholder="닉네임을 입력해주세요."
-          errorMessage="한글 3~8자로 입력해주세요."
-          minLength="3"
-          maxLength="8"
-          value={userData.usernickname}
-          onChange={handleChange}
-        />
-        <FormInput
-          type="password"
-          label="비밀번호"
-          name="userpassword"
-          placeholder="비밀번호를 입력해주세요."
-          errorMessage="영문, 숫자, 특수문자를 포함한 8~16자로 입력해주세요."
-          minLength="8"
-          maxLength="16"
-          value={userData.userpassword}
-          onChange={handleChange}
-        />
-        <FormInput
-          type="password"
-          label="비밀번호 확인"
-          name="userpasswordcheck"
-          placeholder="비밀번호를 다시 입력해주세요."
-          errorMessage="비밀번호가 일치하지 않습니다."
-          minLength="8"
-          maxLength="16"
-          value={userData.userpasswordcheck}
-          onChange={handleChange}
-        />
-        <FormInput
-          type="text"
-          label="범쌤과 닮은 캐릭터의 이름은?"
-          name="answer"
-          placeholder="정답을 입력해주세요."
-          errorMessage="정답이 일치하지 않습니다."
-          value={userData.answer}
-          onChange={handleChange}
-        />
+        <form onSubmit={handleSignUp} className="flex flex-col gap-y-5">
+          <FormInput
+            type="text"
+            name="name"
+            label="이름"
+            placeholder="이름을 입력해주세요."
+            errorMessage="한글 2~5자로 입력해주세요."
+            minLength="2"
+            maxLength="5"
+            value={userData.name}
+            onChange={handleChange}
+          />
+          <FormInput
+            type="email"
+            label="아이디"
+            name="userid"
+            placeholder="이메일 형식으로 입력해주세요."
+            errorMessage="올바른 이메일 형식이 아닙니다."
+            value={userData.userid}
+            onChange={handleChange}
+          />
+          <FormInput
+            type="text"
+            label="닉네임"
+            name="usernickname"
+            placeholder="닉네임을 입력해주세요."
+            errorMessage="한글 3~8자로 입력해주세요."
+            minLength="3"
+            maxLength="8"
+            value={userData.usernickname}
+            onChange={handleChange}
+          />
+          <FormInput
+            type="password"
+            label="비밀번호"
+            name="userpassword"
+            placeholder="비밀번호를 입력해주세요."
+            errorMessage="영문, 숫자, 특수문자를 포함한 8~16자로 입력해주세요."
+            minLength="8"
+            maxLength="16"
+            value={userData.userpassword}
+            onChange={handleChange}
+          />
+          <FormInput
+            type="password"
+            label="비밀번호 확인"
+            name="userpasswordcheck"
+            placeholder="비밀번호를 다시 입력해주세요."
+            errorMessage="비밀번호가 일치하지 않습니다."
+            minLength="8"
+            maxLength="16"
+            value={userData.userpasswordcheck}
+            onChange={handleChange}
+          />
+          <FormInput
+            type="text"
+            label="범쌤과 닮은 캐릭터의 이름은?"
+            name="answer"
+            placeholder="정답을 입력해주세요."
+            errorMessage="정답이 일치하지 않습니다."
+            value={userData.answer}
+            onChange={handleChange}
+          />
 
-        <LinkButton text="가입하기" type="submit" />
-      </form>
-    </div>
+          <LinkButton text="가입하기" type="submit" />
+        </form>
+      </div>
+    </>
   );
 }
+
 export default SignUp;
