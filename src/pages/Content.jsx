3910e30b7d696d9466558843e3cc34062ca0be1b
seumomo <xo0207@naver.com>
2023-09-14 15:38:46 +0900

    [Add, Feat, Refactor] Content 페이지 일부 구현 (2023/09/14)
    1. Content.jsx
      - Feed 페이지에서 클릭한 게시글의 게시글 정보와 작성자 정보 렌더링 구현
    2. index.html
      - noscript 태그 추가
    3. routes.jsx
      - Content 페이지 경로 수정
    4. getFeed.js
      - 게시글 하나만 가져오는 함수 구현
    5. arrow.arrow_common_left.svg
      - 뒤로가기 버튼 이미지 추가
    6. FeedList.jsx
      - styling 일부 수정
      - 이미지의 대체 텍스트 추가
      - 게시글 클릭 시 Content 페이지 연결
    7. useFeed.js
      - useQuery를 사용하여 게시글의 데이터 불러오는 기능 구현
      - 뒤로가기 클릭 시 Content 페이지로 오기 전 활성화 되었던 채널로 이동하는 기능 구현
    8. useInfiniteFeed.js
      - queryKey 이름 수정
    9. getPbImageURL.js
      - Coding Convention에 따라 함수 선언식으로 변경
      - Coding Convention에 따라 기본 내보내기로 설정
    10. utils/index.js
      - getPbImageURL re-export

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -1,5 +1,72 @@
+import { ReactComponent as LeftArrow } from '@/assets/arrow_common_left.svg';
+import useFeed from '@/hooks/useFeed';
+import { getPbImageURL } from '@/utils';
+import getDate from '@/utils/getDate';
+import { Helmet } from 'react-helmet-async';
+import { useNavigate, useParams } from 'react-router-dom';
+
 function Content() {
-  return <div>Content</div>;
+  const { feedId } = useParams();
+  const { status, data, pathname } = useFeed(feedId);
+  const navigate = useNavigate();
+  return (
+    data && (
+      <>
+        <Helmet>
+          <title>Lionly - {data?.expand.author.nickname}의 게시물</title>
+        </Helmet>
+        <h1 className="sr-only">Lionly</h1>
+        <header className="flex px-4 py-3 ">
+          <LeftArrow
+            aria-label="뒤로 가기"
+            role="button"
+            tabIndex="0"
+            onClick={() => {
+              navigate(pathname);
+            }}
+            className="absolute cursor-pointer transition-all hover:scale-125"
+          />
+          <h2 className="w-full text-center text-lionly-base font-bold text-lionly-white">
+            {data?.expand.author.nickname}의 게시물
+          </h2>
+        </header>
+
+        <main className="flex flex-col gap-y-2.5 bg-lionly-white px-4 py-3">
+          <figure className="flex w-full gap-x-3">
+            <img
+              alt={`${data?.expand.author.nickname}의 프로필 이미지`}
+              src={getPbImageURL(data?.expand.author, 'profile_image')}
+              className="h-10 min-h-[40px] w-10 min-w-[40px] rounded-full border-2"
+            />
+            <figcaption>
+              <p className="font-bold text-lionly-black">
+                {data?.expand.author.nickname}
+              </p>
+              <p className="text-lionly-sm text-lionly-gray-1">{`${getDate(
+                data?.created
+              )}`}</p>
+            </figcaption>
+          </figure>
+
+          <figure className="flex w-full flex-col gap-y-[14px]">
+            <img
+              alt="피드 이미지"
+              src={getPbImageURL(data, 'feed_image')}
+              className="aspect-[4/3] w-full self-center rounded-2xl object-cover"
+            />
+
+            <figcaption>
+              <p className="w-full text-lionly-base font-bold text-lionly-gray-1">
+                {data.text}
+              </p>
+            </figcaption>
+          </figure>
+        </main>
+
+        <section></section>
+      </>
+    )
+  );
 }
 
 export default Content;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 export { default as getNodeIndex } from './getNodeIndex';
 export { default as handleTabArrowControl } from './handleTabArrowControl';
 export { default as onLoadMoveScrollTop } from './onLoadMoveScrollTop';
+export { default as getPbImageURL } from './getPbImageURL';
