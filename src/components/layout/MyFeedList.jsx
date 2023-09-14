9de05be2dc9123c80180d60845d788c416cda359
chowonn <hcw12321@gmail.com>
2023-09-14 14:20:47 +0900

    [Modify] 클릭 시 드롭다운 메뉴 보이도록 수정 (2023/09/14)

--- src/components/layout/MyFeedList.jsx
+++ src/components/layout/MyFeedList.jsx
@@ -4,10 +4,14 @@ import getDate from '@/utils/getDate';
 import { getPbImageURL } from '@/utils/getPbImageURL';
 import { shape, string } from 'prop-types';
 import { useState, useEffect } from 'react';
+import { ReactComponent as KebabButtonSVG } from '/src/assets/kebabMenuButton_FeedList.svg';
+import useDropDown from '@/hooks/useDropDown';
 
 function MyFeedList() {
   const { profile_image, email, nickname, id } = useStorageData();
   const [data, setData] = useState(null);
+
+  const [isDropView, dropRef, dropOutHandler] = useDropDown();
 
   useEffect(() => {
     async function myData() {
@@ -19,16 +23,16 @@ function MyFeedList() {
 
   return (
     <>
-      <ul className="m-auto w-72">
+      <ul className="mx-auto flex min-h-[72vh] flex-col gap-y-6 px-2">
         {data?.map((item) => (
           <li key={item.id} className="mb-[22px]">
             <figure className="relative mb-[10px] flex h-10 w-full">
               <img
                 src={getPbImageURL(item.expand.author, 'profile_image')}
-                alt=""
+                aria-hidden
                 className="h-[40px] w-[40px] rounded-full"
               />
-              <figcaption className="ml-3">
+              <figcaption className="ml-3 w-full">
                 <p className="font-bold text-lionly-black">
                   {item.expand.author.nickname}
                 </p>
@@ -36,18 +40,32 @@ function MyFeedList() {
                   {getDate(item.created)}
                 </p>
               </figcaption>
-              <button type="button" className="absolute right-0 h-9 w-9">
-                {/* <KebabButtonSVG /> */}
+              <button
+                type="button"
+                className="absolute right-0 h-9 w-9"
+                onClick={dropOutHandler}
+                ref={dropRef}
+              >
+                <KebabButtonSVG aria-hidden />
               </button>
+              {isDropView && (
+                <ul
+                  ref={dropRef}
+                  className="absolute right-0 top-full z-10 border border-gray-300 bg-white p-2 shadow-lg"
+                >
+                  <li className="cursor-pointer p-2 hover:bg-gray-100">수정</li>
+                  <li className="cursor-pointer p-2 hover:bg-gray-100">삭제</li>
+                </ul>
+              )}
             </figure>
             <figure className="w-full ">
               <img
                 src={getPbImageURL(item, 'feed_image')}
-                alt=""
-                className="h-40 w-full rounded-2xl"
+                aria-hidden
+                className="aspect-[4/3] w-full self-center rounded-2xl object-cover"
               />
               <figcaption className="mt-[14px]">
-                <p className="w-[260] text-lionly-sm text-lionly-gray-1">
+                <p className="w-full text-lionly-md text-lionly-gray-1">
                   {item.text}
                 </p>
               </figcaption>
@@ -64,10 +82,10 @@ MyFeedList.propTypes = {
     id: string,
     image: string,
     nickname: string,
-    date: string,
+    created: string,
     feed_image: string,
     text: string,
-  }).isRequired,
+  }),
 };
 
 export default MyFeedList;
