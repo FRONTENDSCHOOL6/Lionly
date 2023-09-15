b68ee22b45f4d822c23a9048b0a22e3bf6d7b8b3
seumomo <xo0207@naver.com>
2023-09-16 07:48:31 +0900

    [Add] FeedList 정렬 코드 추가 (2023/09/16)

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,4 +3,3 @@ export { default as getPbImageURL } from './getPbImageURL';
 export { default as getTimeDifference } from './getTimeDifference';
 export { default as handleTabArrowControl } from './handleTabArrowControl';
 export { default as onLoadMoveScrollTop } from './onLoadMoveScrollTop';
-export { default as getPbImageURL } from './getPbImageURL';
