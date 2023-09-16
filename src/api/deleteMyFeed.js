f5dcce6fc3d2f3e91f9ded5593a4bd7b9cf88611
chowonn <hcw12321@gmail.com>
2023-09-16 15:42:28 +0900

    [Refactor] id 값 전달 및 알림창 추가 (2023/09/16)

--- src/api/deleteMyFeed.js
+++ src/api/deleteMyFeed.js
@@ -1,7 +1,15 @@
+import toast from 'react-hot-toast';
 import pb from './pocketbase';
 
-async function deleteMyFeed() {
-  const MyFeedItem = await pb.collection('feeds').delete();
-  return MyFeedItem;
+async function deleteMyFeed(id) {
+  const deleteFeed = window.confirm(
+    '삭제된 글은 복구하실 수 없습니다. 삭제하시겠습니까?'
+  );
+  if (deleteFeed) {
+    await pb.collection('feeds').delete(`${id}`);
+    toast('삭제되었습니다');
+    location.reload();
+  }
+  return deleteFeed;
 }
 export default deleteMyFeed;
