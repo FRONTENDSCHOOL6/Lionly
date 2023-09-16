9eb12e78c7980562f8ffa56a58b91ca21ccacb16
itzwe <itzwewewe@naver.com>
2023-09-17 02:13:16 +0900

    [Add] 로그인 페이지 helmet 추가

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,11 +1,19 @@
-import Login from "@/components/layout/Login";
+import Login from '@/components/layout/Login';
+import { Helmet } from 'react-helmet-async';
 
 function SignIn() {
   return (
-    <div className="mx-[35px]">
-      <h2 className="mt-[30px] text-lionly-2xl text-lionly-white pb-4">로그인</h2>
-      <Login/>
-    </div>
+    <>
+      <Helmet>
+        <title>Lionly - Signin</title>
+      </Helmet>
+      <div className="mx-[35px]">
+        <h2 className="mt-[30px] pb-4 text-lionly-2xl text-lionly-white">
+          로그인
+        </h2>
+        <Login />
+      </div>
+    </>
   );
 }
 
