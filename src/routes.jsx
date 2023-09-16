cf130053d26b515557f1e080324bb79b3e390906
itzwe <itzwewewe@naver.com>
2023-09-17 01:06:24 +0900

    [Add] routes 수정

--- src/routes.jsx
+++ src/routes.jsx
@@ -49,7 +49,7 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path: '/edit',
+        path: '/edit/:postId',
         element: (
           <ProtectedRoute>
             <Edit />
