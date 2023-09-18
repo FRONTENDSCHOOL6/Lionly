fbef3534f6e0285ce87968965ea531d974753249
leejisu <ejeesoo97@gmail.com>
2023-09-18 14:36:28 +0900

    [Modify] 유효성 검사 코드 수정 - 빈칸일 때 알림 메세지 추가 - 정답 입력창 코드 추가 (2023/09/18)

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -24,8 +24,14 @@ function SignUp() {
     e.preventDefault();
 
     try {
-      const { name, userid, usernickname, userpassword, userpasswordcheck } =
-        userData;
+      const {
+        name,
+        userid,
+        usernickname,
+        userpassword,
+        userpasswordcheck,
+        answer,
+      } = userData;
 
       const newUser = {
         email: userid,
@@ -34,18 +40,35 @@ function SignUp() {
         nickname: usernickname,
         password: userpassword,
         passwordConfirm: userpasswordcheck,
+        answer: answer,
       };
 
+      if (name === '') {
+        toast.error('이름을 입력해주세요.', { icon: '⚠️' });
+        return;
+      }
       if (!nameReg(name)) {
         toast.error('이름 형식이 올바르지 않습니다.');
+        return;
+      }
+      if (userid === '') {
+        toast.error('이메일을 입력해주세요.', { icon: '⚠️' });
         return;
       }
       if (!idReg(userid)) {
         toast.error('이메일 형식이 올바르지 않습니다.');
         return;
       }
+      if (usernickname === '') {
+        toast.error('닉네임을 입력해주세요.', { icon: '⚠️' });
+        return;
+      }
       if (!nickNameReg(usernickname)) {
         toast.error('닉네임 형식이 올바르지 않습니다.');
+        return;
+      }
+      if (userpassword === '') {
+        toast.error('비밀번호를 입력해주세요.', { icon: '⚠️' });
         return;
       }
       if (!passWordReg(userpassword)) {
@@ -54,6 +77,14 @@ function SignUp() {
       }
       if (userpassword !== userpasswordcheck) {
         toast.error('비밀번호와 비밀번호 확인이 일치하지 않습니다.');
+        return;
+      }
+      if (answer === '') {
+        toast.error('정답을 입력해주세요.', { icon: '⚠️' });
+        return;
+      }
+      if (answer !== '카레빵맨') {
+        toast.error('정답이 일치하지 않습니다.');
         return;
       }
 
