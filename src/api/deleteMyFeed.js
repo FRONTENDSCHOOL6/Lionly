82f0a6327a0b494cf738ad52bd8d5ef30ebe71d9
chowonn <hcw12321@gmail.com>
2023-09-19 02:09:05 +0900

    [Feat] 댓글까지 삭제되도록 조건 추가 (2023/09/19)

--- src/api/deleteMyFeed.js
+++ src/api/deleteMyFeed.js
@@ -1,12 +1,15 @@
 import toast from 'react-hot-toast';
 import pb from './pocketbase';
 
-async function deleteMyFeed(id) {
+async function deleteMyFeed(id, commentArray) {
   const deleteFeed = window.confirm(
     '삭제된 글은 복구하실 수 없습니다. 삭제하시겠습니까?'
   );
   if (deleteFeed) {
     await pb.collection('feeds').delete(`${id}`);
+    for (let i = 0; i < commentArray.length; i++) {
+      await pb.collection('comments').delete(commentArray[i]);
+    }
     toast('삭제되었습니다');
     location.reload();
   }
