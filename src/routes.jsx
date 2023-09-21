8c1887866812b931e70235460e521591541a27e0
seumomo <xo0207@naver.com>
2023-09-21 10:34:19 +0900

    [Modify] signin 경로 수정 (2023/09/21)

--- src/routes.jsx
+++ src/routes.jsx
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
             element: <Landing />,
           },
           {
-            path: 'signin',
+            path: '/signin',
             element: <SignIn />,
           },
           {
