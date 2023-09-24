92dd4b6d1e10f7b85418a0c44f8af8dd63f8535a
seumomo <xo0207@naver.com>
2023-09-25 03:22:57 +0900

    [Refactor] scrollTo 복원 (2023/09/25)

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { AnimatePresence } from 'framer-motion';
 import { Suspense } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'react-hot-toast';
-import { RouterProvider, ScrollRestoration } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import Spinner from './components/Spinner';
 import { AuthProvider, ChannelProvider, ContentProvider } from './contexts';
 import router from './routes.jsx';
@@ -34,7 +34,6 @@ function App() {
                       }
                     >
                       <RouterProvider router={router} />
-                      <ScrollRestoration />
                     </Suspense>
                   </div>
                 </AnimatePresence>
