3418180bf5085b01116c7e6dff951e537a13ea39
TAEIL KIM <xo0207@naver.com>
2023-09-25 17:43:06 +0900

    Revert "[PR] 최종 merge (#128)" (#130)
    This reverts commit b27e9dce50f25d49c9c320c57841ab5ff6301f0f.

--- src/api/index.js
+++
@@ -1,5 +0,0 @@
-export { default as createData } from './createData.js';
-export { default as deleteMyFeed } from './deleteMyFeed.js';
-export { default as getContent } from './getContent.js';
-export { default as getFeedList } from './getFeedList.js';
-export { default as getMyFeedList } from './getMyFeedList.js';
--- src/utils/renderImg.js
+++
@@ -1,5 +0,0 @@
-function renderImg(collection, id, value) {
-  return `https://lionly.pockethost.io/api/files/${collection}/${id}/${value}`;
-}
-
-export default renderImg;
